e5723b5028fdd906044e2e1ba3ce600844860f9a
Stanislav SHumkov <66177661+borz7zy@users.noreply.github.com>
2025-11-21 21:41:12 +0100

    adapted for c++20

--- Source/Getche.cpp
+++ Source/Getche.cpp
@@ -8,28 +8,41 @@
  *
  */
 
-#if   defined(_WIN32)
-#include <conio.h> /* getche() */
-#elif  defined(__S3E__)
+#if defined(_WIN32)
+
+#include <conio.h> // getche()
+
+#elif defined(__S3E__)
 
 #else
 
 #include "../include/RakNet/Getche.h"
+#include <termios.h>
+#include <unistd.h>
+#include <cstdio>
 
 char getche()
 {
+    termios oldt{};
+    termios newt{};
 
+    if (tcgetattr(STDIN_FILENO, &oldt) == -1)
+        return '\0';
 
-  struct termios oldt,
-                 newt;
-  char            ch;
-  tcgetattr( STDIN_FILENO, &oldt );
-  newt = oldt;
-  newt.c_lflag &= ~( ICANON | ECHO );
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt );
-  ch = getchar();
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
-  return ch;
+    newt = oldt;
+    newt.c_lflag &= static_cast<unsigned>(~(ICANON | ECHO));
 
-} 
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &newt) == -1)
+        return '\0';
+
+    int ch = std::getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    if (ch == EOF)
+        return '\0';
+
+    return static_cast<char>(ch);
+}
+
 #endif
